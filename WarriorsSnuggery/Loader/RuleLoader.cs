6a8a5d7835ce9b31f35156f0720599561bb8aa9b
abc013 <abc013@alpenjodel.de>
2019-06-08 18:05:32 +0200

    Fixed some physic engine problems

--- WarriorsSnuggery/Loader/RuleLoader.cs
+++ WarriorsSnuggery/Loader/RuleLoader.cs
@@ -8,7 +8,7 @@ namespace WarriorsSnuggery
 	{
 		public static void LoadRules()
 		{
-			var rules = RuleReader.Read(FileExplorer.Rules, "Rules.yaml"); //TODO remove OR use for mods?
+			var rules = RuleReader.Read(FileExplorer.Rules, "Rules.yaml"); //TODO for future: use for mods
 
 			foreach (var rule in rules)
 			{
