2af344b3ef09989a10742ff0fff496b0c9c52cad
abc013 <abc013@alpenjodel.de>
2019-09-18 21:24:48 +0200

    Improved texture loading technique

---
+++ WarriorsSnuggery/Graphics/TerrainSpriteManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WarriorsSnuggery.Graphics
+{
+	public class TerrainSpriteManager
+	{
+		public static int TextureID;
+
+		// ´byte 1,2: ID; 3: offset; 4: type;
+		static readonly Dictionary<uint, ImageRenderable> sprites = new Dictionary<uint, ImageRenderable>();
+
+		public static void AddTexture(TextureInfo info, ushort ID, byte offset, byte type)
+		{
+			
+		}
+	}
+}
---
+++ WarriorsSnuggery/Loader/BitmapLoader.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace WarriorsSnuggery.Loader
+{
+	public static class BitmapLoader
+	{
+		public static float[] LoadTexture(string filename, out int width, out int height)
+		{
+			if (!File.Exists(filename))
+				throw new FileNotFoundException("The file `" + filename + "` has not been found.");
+
+			float[] r;
+
+			using (var bitmap = new Bitmap(filename))
+			{
+				width = bitmap.Width;
+				height = bitmap.Height;
+				r = LoadTexture(bitmap, new Rectangle(Point.Empty, bitmap.Size));
+			}
+
+			return r;
+		}
+
+		// https://stackoverflow.com/questions/4747428/getting-rgb-array-from-image-in-c-sharp original
+		public static float[] LoadTexture(Bitmap bmp, Rectangle size)
+		{
+			const int pixelWidth = 4;
+
+			var data = bmp.LockBits(size, ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+
+			var scansize = data.Width * 4;
+			var stride = data.Stride;
+			var r = new float[data.Height * stride];
+			try
+			{
+				var scan = new byte[stride];
+				for (int scanline = 0; scanline < data.Height; scanline++)
+				{
+					Marshal.Copy(data.Scan0 + scanline * stride, scan, 0, stride);
+					for (int px = 0; px < data.Width; px++)
+					{
+						// little endian
+						// B
+						r[scanline * scansize + px * pixelWidth + 2] = scan[px * pixelWidth] / 255f;
+						// G
+						r[scanline * scansize + px * pixelWidth + 1] = scan[px * pixelWidth + 1] / 255f;
+						// R
+						r[scanline * scansize + px * pixelWidth] = scan[px * pixelWidth + 2] / 255f;
+						// A
+						r[scanline * scansize + px * pixelWidth + 3] = scan[px * pixelWidth + 3] / 255f;
+					}
+				}
+			}
+			finally
+			{
+				bmp.UnlockBits(data);
+			}
+
+			return r;
+		}
+	}
+}
