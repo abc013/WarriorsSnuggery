8212c0c91d7f4ea372ac6672abb2559fab47e58e
abc013 <abc013@alpenjodel.de>
2019-10-04 16:47:22 +0200

    Decrease maximum map bounds

--- WarriorsSnuggery/Map/MapUtils.cs
+++ WarriorsSnuggery/Map/MapUtils.cs
@@ -7,7 +7,7 @@ namespace WarriorsSnuggery.Maps
 	public static class MapUtils
 	{
 		public static readonly MPos MinimumMapBounds = new MPos(16, 16);
-		public static readonly MPos MaximumMapBounds = new MPos(256, 256);
+		public static readonly MPos MaximumMapBounds = new MPos(128, 128);
 
 		public static MPos[] FindValuesInArea(MPos position, int searchRadius, int[] value, int[,] array, MPos bounds)
 		{
