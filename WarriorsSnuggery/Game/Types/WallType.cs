380be427a30b5d9510bcc788b03f1449f9a8cc8a
abc013 <abc013@alpenjodel.de>
2019-11-16 13:25:58 +0100

    Reworked wall loading

--- WarriorsSnuggery/Game/Types/WallType.cs
+++ WarriorsSnuggery/Game/Types/WallType.cs
@@ -6,27 +6,31 @@ namespace WarriorsSnuggery.Objects
 	{
 		public readonly int ID;
 
-		[Desc("Texture of the wall", "Use \"Image\" as rule.")]
+		[Desc("Texture of the wall.")]
+		public readonly string Image;
+
 		readonly IImage[] textures;
 
 		[Desc("If yes, this wall will block objects with physics.")]
-		public readonly bool Blocks;
-		[Desc("If yes, the wall can be destroyed. Unused.")]
-		public readonly bool Destructable;
+		public readonly bool Blocks = true;
 		[Desc("Height of the wall.")]
-		public readonly int Height;
+		public readonly int Height = 1024;
 
-		public WallType(int id, string texture, bool blocks, bool destructable, int height)
+		[Desc("Health of the wall.")]
+		public readonly int Health = -1;
+
+		public WallType(int id, MiniTextNode[] nodes)
 		{
 			ID = id;
-			textures = SpriteManager.AddTexture(new TextureInfo(texture, TextureType.ANIMATION, 0, 24, 48));
+			Loader.PartLoader.SetValues(this, nodes);
+
+			if (Image == null)
+				throw new YamlMissingNodeException("[Wall] " + id, "Image");
+
+			textures = SpriteManager.AddTexture(new TextureInfo(Image, TextureType.ANIMATION, 0, 24, 48));
 
 			if (textures.Length < 2)
-				throw new YamlInvalidNodeException(string.Format("Texture '{0}' of Wall '{1}' has not enough textures!", texture, id));
-
-			Blocks = blocks;
-			Destructable = destructable;
-			Height = height;
+				throw new YamlInvalidNodeException(string.Format("Texture '{0}' of Wall '{1}' has not enough textures!", Image, id));
 		}
 
 		public IImage GetTexture(bool horizontal)
