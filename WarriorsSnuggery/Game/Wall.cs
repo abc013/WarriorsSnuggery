c1b04c48ae6d2e9379f13ee87503e95086803a90
abc013 <abc013@alpenjodel.de>
2019-11-16 13:53:35 +0100

    Fix walls appearing too late

--- WarriorsSnuggery/Game/Wall.cs
+++ WarriorsSnuggery/Game/Wall.cs
@@ -12,15 +12,25 @@ namespace WarriorsSnuggery.Objects
 		public readonly WPos LayerPosition;
 		readonly CPos renderPosition;
 		public readonly WallType Type;
+		readonly bool isHorizontal;
 
 		public Wall(WPos position, WallType type) : base(position.ToCPos(), new WallRenderable(position.X % 2 != 0, type), type.Blocks ? new Physics.SimplePhysics(position.ToCPos() / new CPos(2, 1, 1), 0, position.X % 2 != 0 ? WarriorsSnuggery.Physics.Shape.LINE_HORIZONTAL : WarriorsSnuggery.Physics.Shape.LINE_VERTICAL, 512, 512, type.Height) : new Physics.SimplePhysics(position.ToCPos() / new CPos(2, 1, 1), 0, WarriorsSnuggery.Physics.Shape.NONE, 0, 0, 0))
 		{
 			LayerPosition = position;
 			var pos = position.ToCPos() / new CPos(2, 1, 1);
 			Position = pos + new CPos(0, -512, 0);
-			Position += position.X % 2 != 0 ? CPos.Zero : new CPos(0, 0, 2048);
-			Physics.Position += position.X % 2 != 0 ? new CPos(0, 512, 0) : new CPos(0, 1024, 0);
-			renderPosition = pos + (position.X % 2 != 0 ? new CPos(-512, -1536, 0) : new CPos(-83, -512, 0));
+			isHorizontal = position.X % 2 != 0;
+			if (isHorizontal)
+			{
+				Physics.Position += new CPos(0, 512, 0);
+				renderPosition = pos + new CPos(-512, -1536, 0);
+			}
+			else
+			{
+				Position += new CPos(0, 0, 2048);
+				Physics.Position += new CPos(0, 1024, 0);
+				renderPosition = pos + new CPos(-83, -512, 0);
+			}
 			Type = type;
 		}
 
@@ -37,7 +47,10 @@ namespace WarriorsSnuggery.Objects
 
 		public override void CheckVisibility()
 		{
-			Renderable.Visible = VisibilitySolver.IsVisibleIgnoringBounds(new WPos(LayerPosition.X / 2, LayerPosition.Y, 0));
+			Renderable.Visible = VisibilitySolver.IsVisibleIgnoringBounds(new WPos(LayerPosition.X / 2, LayerPosition.Y - 1, 0));
+
+			if (!isHorizontal)
+				Renderable.Visible |= VisibilitySolver.IsVisibleIgnoringBounds(new WPos(LayerPosition.X / 2, LayerPosition.Y, 0));
 		}
 	}
 }
