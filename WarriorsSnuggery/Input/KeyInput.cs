d3fa8f1a8cfdb3246abffef6920e8fd33e3f41e0
abc013 <abc013@alpenjodel.de>
2020-04-14 02:58:57 +0200

    Remove unused code

--- WarriorsSnuggery/Input/KeyInput.cs
+++ WarriorsSnuggery/Input/KeyInput.cs
@@ -8,43 +8,6 @@ namespace WarriorsSnuggery
 	{
 		public readonly static char[] InvalidFileNameChars;
 
-		public readonly static string[] AllKeys =
-		{
-			"↓",
-			"↑",
-			"←",
-			"→",
-			"-",
-			"+",
-			",",
-			"q",
-			"w",
-			"e",
-			"r",
-			"t",
-			"y",
-			"u",
-			"i",
-			"o",
-			"p",
-			"a",
-			"s",
-			"d",
-			"f",
-			"g",
-			"h",
-			"j",
-			"k",
-			"l",
-			"z",
-			"x",
-			"c",
-			"v",
-			"b",
-			"n",
-			"m"
-		};
-
 		static KeyboardState state;
 		public static int HitCooldown;
 
@@ -58,33 +21,6 @@ namespace WarriorsSnuggery
 			if (HitCooldown > 0 || !state.IsAnyKeyDown || !WindowInfo.Focused)
 				return false;
 
-			if (key.IndexOfAny(new[] { '↓', '↑', '←', '→', '-', '+', ',' }) == 0)
-			{
-				switch (key)
-				{
-					case "↓":
-						key = "down";
-						break;
-					case "↑":
-						key = "up";
-						break;
-					case "←":
-						key = "left";
-						break;
-					case "→":
-						key = "right";
-						break;
-					case "-":
-						key = "minus";
-						break;
-					case "+":
-						key = "plus";
-						break;
-					case ",":
-						key = "comma";
-						break;
-				}
-			}
 			bool hit = state.IsKeyDown((Key)Enum.Parse(typeof(Key), key, true));
 
 			if (hit)
