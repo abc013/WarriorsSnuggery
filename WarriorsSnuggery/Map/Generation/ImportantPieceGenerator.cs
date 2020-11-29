38c4ff577d451d3aed7c4cf9ffaabf52d6615b05
abc013 <abc013@alpenjodel.de>
2020-11-29 21:27:41 +0100

    Add more options to the ImportantMapGenerator and make generation more diverse

--- WarriorsSnuggery/Map/Generation/ImportantPieceGenerator.cs
+++ WarriorsSnuggery/Map/Generation/ImportantPieceGenerator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace WarriorsSnuggery.Maps
 {
@@ -7,7 +8,8 @@ namespace WarriorsSnuggery.Maps
 	{
 		POSITION,
 		SPAWN,
-		EXIT
+		EXIT,
+		KEY
 	}
 
 	[Desc("Generator used to generate pieces that must be on the map.")]
@@ -16,13 +18,19 @@ namespace WarriorsSnuggery.Maps
 		[Desc("Unique ID for the generator.")]
 		public readonly new int ID;
 
+		[Desc("NoiseMap that influences the placement of the piece. There will always be a little chance (10%) to ensure that the piece is placed.", "This setting only influences 'EXIT' and 'KEY' position modes.")]
+		public readonly int NoiseMapID = -1;
+
 		[Desc("Pieces of which one is chosen to be spawned.")]
 		public readonly string[] Pieces = new string[0];
 
-		[Desc("Position on the map, if PositionType is set on 'POSITION'.")]
+		[Desc("Sets where to spawn the piece. This option only works if PositionType is set to 'POSITION'.")]
 		public readonly MPos Position = MPos.Zero;
+
 		[Desc("Position type.")]
 		public readonly PositionType PositionType = PositionType.POSITION;
+		[Desc("Determines when to generate this piece. If set to NONE, it will always be generated.")]
+		public readonly GameMode[] SpawnOnMode = new [] { GameMode.NONE };
 
 		public ImportantPieceGeneratorInfo(int id, List<MiniTextNode> nodes) : base(id)
 		{
@@ -48,6 +56,11 @@ namespace WarriorsSnuggery.Maps
 
 		public override void Generate()
 		{
+			var noise = GeneratorUtils.GetNoise(loader, info.NoiseMapID);
+
+			if (!info.SpawnOnMode.Contains(GameMode.NONE) && !info.SpawnOnMode.Contains(loader.GameMode))
+				return;
+
 			var pieceIndex = info.Pieces[random.Next(info.Pieces.Length)];
 			Piece = PieceManager.GetPiece(pieceIndex);
 			switch (info.PositionType)
@@ -56,16 +69,18 @@ namespace WarriorsSnuggery.Maps
 					loader.GenerateCrucialPiece(Piece, info.Position, info.ID);
 					break;
 				case PositionType.SPAWN:
-					spawnSpawn();
+					generateSpawn();
+					break;
+				case PositionType.KEY:
+					generateKey(noise);
 					break;
 				case PositionType.EXIT:
-					if (loader.GameMode == GameMode.FIND_EXIT)
-						exitSpawn();
+					generateExit(noise);
 					break;
 			}
 		}
 
-		void spawnSpawn()
+		void generateSpawn()
 		{
 			var spawnArea = Bounds - Piece.Size;
 			var half = spawnArea / new MPos(2, 2);
@@ -76,33 +91,74 @@ namespace WarriorsSnuggery.Maps
 			loader.PlayerSpawn = new CPos(pos.X * 1024 + Piece.Size.X * 512, pos.Y * 1024 + Piece.Size.Y * 512, 0);
 		}
 
-		void exitSpawn()
+		void generateKey(float[] noise)
+		{
+			var exitExists = loader.Exit == CPos.Zero;
+			var mapLength = Bounds.Dist * 256;
+
+			var dist = random.Next(8);
+			var spawnArea = Bounds - (Piece.Size + new MPos(dist, dist));
+
+			MPos pos;
+			var success = false;
+			do
+			{
+				pos = getPosNearBorder(spawnArea);
+
+				// Don't spawn near exits
+				if (exitExists && (pos.ToCPos() - loader.Exit).SquaredFlatDist < mapLength)
+					continue;
+
+				if (info.NoiseMapID >= 0 && random.NextDouble() > noise[pos.Y * Bounds.X + pos.X] + 0.1f)
+					continue;
+
+				success = loader.GeneratePiece(Piece, pos, 100, true);
+			}
+			while (!success);
+		}
+
+		void generateExit(float[] noise)
 		{
 			var spawnArea = Bounds - Piece.Size;
-			var pos = new MPos(-1, -1);
 
-			while (pos.X < 0 || !loader.GeneratePiece(Piece, pos, 100, true))
+			MPos pos;
+			var success = false;
+			do
 			{
-				// Picking a random side, 0 = x, 1 = y, 2 = -x, 3 = -y;
-				var side = (byte)random.Next(4);
-				switch (side)
-				{
-					case 0:
-						pos = new MPos(random.Next(2), random.Next(spawnArea.X));
-						break;
-					case 1:
-						pos = new MPos(random.Next(spawnArea.Y), random.Next(2));
-						break;
-					case 2:
-						pos = new MPos(spawnArea.X - random.Next(2), random.Next(spawnArea.X));
-						break;
-					case 3:
-						pos = new MPos(random.Next(spawnArea.X), spawnArea.Y - random.Next(2));
-						break;
-				}
+				pos = getPosNearBorder(spawnArea);
+
+				if (info.NoiseMapID >= 0 && random.NextDouble() > noise[pos.Y * Bounds.X + pos.X] + 0.1f)
+					continue;
+
+				success = loader.GeneratePiece(Piece, pos, 100, true);
 			}
+			while (!success);
 
 			loader.Exit = pos.ToCPos() + new CPos(Piece.Size.X * 512, Piece.Size.Y * 512, 0);
+		}
+
+		MPos getPosNearBorder(MPos spawnArea)
+		{
+			var pos = MPos.Zero;
+			// Picking a random side, 0 = x, 1 = y, 2 = -x, 3 = -y;
+			var side = (byte)random.Next(4);
+			switch (side)
+			{
+				case 0:
+					pos = new MPos(random.Next(2), random.Next(spawnArea.X));
+					break;
+				case 1:
+					pos = new MPos(random.Next(spawnArea.Y), random.Next(2));
+					break;
+				case 2:
+					pos = new MPos(spawnArea.X - random.Next(2), random.Next(spawnArea.X));
+					break;
+				case 3:
+					pos = new MPos(random.Next(spawnArea.X), spawnArea.Y - random.Next(2));
+					break;
+			}
+
+			return pos;
 		}
 	}
 }
