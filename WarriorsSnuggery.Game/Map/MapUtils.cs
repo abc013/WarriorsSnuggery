de00bca4fd64932e622d3f9cff64bd56b4972079
abc013 <abc013@users.noreply.github.com>
2021-03-22 13:30:57 +0100

    Reduce minimum map bounds to 32

--- WarriorsSnuggery.Game/Map/MapUtils.cs
+++ WarriorsSnuggery.Game/Map/MapUtils.cs
@@ -6,7 +6,7 @@ namespace WarriorsSnuggery.Maps
 {
 	public static class MapUtils
 	{
-		public static readonly MPos MinimumMapBounds = new MPos(64, 64);
+		public static readonly MPos MinimumMapBounds = new MPos(32, 32);
 		public static readonly MPos MaximumMapBounds = new MPos(512, 512);
 
 		public static MPos[] FindValuesInArea(MPos position, int searchRadius, int[] value, int[,] array, MPos bounds)
