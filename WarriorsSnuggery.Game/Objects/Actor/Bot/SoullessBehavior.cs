b80e6a3e2328dc719600aa6c993d0e6833f0e032
abc013 <abc013@alpenjodel.de>
2021-11-15 00:14:25 +0100

    Add Soulless Botbehavior

---
+++ WarriorsSnuggery.Game/Objects/Actor/Bot/SoullessBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using WarriorsSnuggery.Loader;
+
+namespace WarriorsSnuggery.Objects.Actors.Bot
+{
+	[Desc("Type that shows no self-initialized behavior at all.")]
+	public class SoullessBotBehaviorType : BotBehaviorType
+	{
+		public SoullessBotBehaviorType(List<TextNode> nodes) : base(nodes) { }
+
+		public override BotBehavior Create(Actor self)
+		{
+			return new SoullessBotBehavior(self, this);
+		}
+	}
+
+	public class SoullessBotBehavior : BotBehavior
+	{
+		public SoullessBotBehavior(Actor self, SoullessBotBehaviorType type) : base(self) { }
+
+		public override void Tick()
+		{
+			if (!CanMove)
+				return;
+
+			DefaultMoveBehavior(0, 0.1f);
+		}
+	}
+}
