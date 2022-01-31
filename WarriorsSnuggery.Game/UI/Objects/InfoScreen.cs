683d9da2a3e089042ac09045b4c890418be7afd2
abc013 <abc013@alpenjodel.de>
2022-01-31 04:06:13 +0100

    Simplify InfoScreen

--- WarriorsSnuggery.Game/UI/Objects/InfoScreen.cs
+++ WarriorsSnuggery.Game/UI/Objects/InfoScreen.cs
@@ -7,9 +7,7 @@ namespace WarriorsSnuggery.UI.Objects
 		public bool Visible => Settings.EnableInfoScreen;
 
 		readonly UIText version;
-		readonly UIText visibility;
-		readonly UIText tick;
-		readonly UIText render;
+		readonly UIText debug;
 
 		public InfoScreen()
 		{
@@ -20,32 +18,25 @@ namespace WarriorsSnuggery.UI.Objects
 				Color = Color.Yellow
 			};
 			version.SetText(Settings.Version);
-			visibility = new UIText(FontManager.Default, TextOffset.RIGHT) { Position = new CPos(corner, 6692, 0) };
-			tick = new UIText(FontManager.Default, TextOffset.RIGHT) { Position = new CPos(corner, 7692, 0) };
-			render = new UIText(FontManager.Default, TextOffset.RIGHT) { Position = new CPos(corner, 7192, 0) };
+			debug = new UIText(FontManager.Default, TextOffset.RIGHT) { Position = new CPos(corner, 6692, 0) };
 		}
 
 		public void Tick()
 		{
-			if (Visible)
+			if (Visible || Window.GlobalTick % 10 != 0)
 				return;
 
 			//memory.SetText("Memory " + (int) (System.Diagnostics.Process.GetCurrentProcess().PrivateMemorySize64 / 1024f) + " KB");
 			//memory.SetText("Public Memory " + (int)(GC.GetTotalMemory(false) / 1024f) + " KB");
-			visibility.SetText(CameraVisibility.TilesVisible() + " Tiles visible");
-
-			if (Window.GlobalTick % 10 != 0)
-				return;
+			debug.SetText(CameraVisibility.TilesVisible() + " Tiles visible");
 
 			var tps = PerfInfo.AverageTPS();
-			tick.Color = getColor(tps, Settings.UpdatesPerSecond);
-			tick.SetText("Tick " + tps.ToString("F1") + " @ " + PerfInfo.TMS.ToString("00") + " ms");
+			debug.AddText(getColor(tps, Settings.UpdatesPerSecond) + $"Tick {tps.ToString("F1")} @ {PerfInfo.TMS:00} ms");
 
 			var frameCount = Settings.FrameLimiter == 0 ? ScreenInfo.ScreenRefreshRate : Settings.FrameLimiter;
 
 			var fps = PerfInfo.AverageFPS();
-			render.Color = getColor(fps, frameCount);
-			render.SetText("Render " + fps.ToString("F1") + " @ " + PerfInfo.FMS.ToString("00") + " ms");
+			debug.AddText(getColor(fps, frameCount) + $"Render {fps.ToString("F1")} @ {PerfInfo.FMS:00} ms");
 		}
 
 		public void Render()
@@ -57,18 +48,16 @@ namespace WarriorsSnuggery.UI.Objects
 			ColorManager.DrawRect(new CPos(right, 8192, 0), new CPos(right - 6144, 8192 - 2560, 0), new Color(0, 0, 0, 128));
 			
 			version.Render();
-			visibility.Render();
-			tick.Render();
-			render.Render();
+			debug.Render();
 		}
 
 		Color getColor(double value, double average)
 		{
 			if (value < average - 5)
-				return new Color(255, 192, 192);
+				return new Color(255, 128, 128);
 
 			if (value > average + 5)
-				return new Color(192, 255, 192);
+				return new Color(128, 255, 128);
 
 			return Color.White;
 		}
