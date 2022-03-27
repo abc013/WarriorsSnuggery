344054c16093111afbf8ddda5c6dae265c4aa7f2
abc013 <abc013@alpenjodel.de>
2022-03-27 18:47:26 +0200

    Add splash weapon

---
+++ WarriorsSnuggery.Game/Objects/Weapons/Projectiles/SplashProjectile.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using WarriorsSnuggery.Graphics;
+using WarriorsSnuggery.Loader;
+using WarriorsSnuggery.Objects.Particles;
+
+namespace WarriorsSnuggery.Objects.Weapons.Projectiles
+{
+	public class SplashProjectile : IProjectile
+	{
+		[Require, Desc("Texture of the Weapon.")]
+		public readonly TextureInfo Texture;
+		[Desc("Ignore ambient lighting for the texture.")]
+		public readonly bool IgnoreAmbience;
+
+		[Desc("Particles that are emitted at the weapons travel time.")]
+		public readonly ParticleSpawner TrailParticles;
+
+		[Desc("Inaccuracy of the weapon.")]
+		public readonly int Inaccuracy;
+
+		[Desc("Don't despawn if collision with another object occurs.")]
+		public readonly bool IgnoreCollisions;
+
+		[Desc("Sets the distance in such a way that all explosions occur between origin and target.")]
+		public readonly bool DistanceBasedOnTarget;
+
+		[Desc("Maximal number of explosions that are triggered.")]
+		public readonly int Repetitions;
+
+		[Desc("Delay between single explosions.")]
+		public readonly int RepetitionDelay = 32;
+
+		[Desc("Distance between explosions.", $"Can be overriden by {nameof(DistanceBasedOnTarget)}.")]
+		public readonly int RepetitionDistance = 512;
+
+		public int DistancePerTick => RepetitionDistance / RepetitionDelay;
+
+		public SplashProjectile(List<TextNode> nodes)
+		{
+			TypeLoader.SetValues(this, nodes);
+		}
+
+		public BatchSequence GetTexture()
+		{
+			var sequence = new BatchSequence(Texture);
+			sequence.SetTextureFlags(IgnoreAmbience ? TextureFlags.IgnoreAmbience : TextureFlags.None);
+			return sequence;
+		}
+	}
+}
---
+++ WarriorsSnuggery.Game/Objects/Weapons/SplashWeapon.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using WarriorsSnuggery.Objects.Actors;
+using WarriorsSnuggery.Objects.Weapons.Projectiles;
+using WarriorsSnuggery.Physics;
+
+namespace WarriorsSnuggery.Objects.Weapons
+{
+	class SplashWeapon : Weapon
+	{
+		readonly SplashProjectile projectile;
+		readonly PhysicsRay ray;
+
+		[Save("Repetition")]
+		int repetition;
+		[Save("RepetitionDelay")]
+		int repetitionDelay;
+
+		[Save("Speed")]
+		readonly CPos speed;
+
+		public SplashWeapon(World world, WeaponType type, Target target, Actor origin, uint id) : base(world, type, target, origin, id)
+		{
+			projectile = (SplashProjectile)type.Projectile;
+
+			Angle = (Position - TargetPosition).FlatAngle;
+			speed = calculateSpeed();
+
+			repetition = projectile.Repetitions;
+			repetitionDelay = projectile.RepetitionDelay;
+
+			ray = new PhysicsRay(world);
+		}
+
+		public SplashWeapon(World world, WeaponInit init) : base(world, init)
+		{
+			projectile = (SplashProjectile)Type.Projectile;
+
+			Angle = (Position - TargetPosition).FlatAngle;
+			speed = init.Convert("Speed", CPos.Zero);
+
+			repetition = init.Convert("Repetition", 0);
+			repetitionDelay = init.Convert("RepetitionDelay", 0);
+
+			ray = new PhysicsRay(world);
+		}
+
+		CPos calculateSpeed()
+		{
+			var dist = projectile.DistanceBasedOnTarget ? (int)(Position - TargetPosition).FlatDist/(projectile.RepetitionDelay * projectile.Repetitions) : projectile.DistancePerTick;
+			return CPos.FromFlatAngle(Angle, dist);
+		}
+
+		public override void Tick()
+		{
+			base.Tick();
+
+			if (World.Game.Editor)
+				return;
+
+			Move();
+
+			if (repetitionDelay-- <= 0)
+			{
+				repetitionDelay = projectile.RepetitionDelay;
+				var random = CPos.FromFlatAngle(WarriorsSnuggery.Angle.RandomAngle(World.Game.SharedRandom), projectile.Inaccuracy);
+				Detonate(new Target(Position + random, Height), repetition-- == 0);
+			}
+
+			if (projectile.TrailParticles != null)
+				World.Add(projectile.TrailParticles.Create(World, Position, Height));
+		}
+
+		public void Move()
+		{
+			var beforePos = Position;
+			var beforeHeight = Height;
+
+			DistanceTravelled += (int)speed.FlatDist;
+
+			Position += speed;
+			Height += speed.Z;
+
+			if (Height == 0 && speed.Z < 0 || !World.IsInWorld(Position))
+				Detonate(new Target(Position, 0));
+
+			if (projectile.IgnoreCollisions)
+				return;
+
+			ray.Start = beforePos;
+			ray.StartHeight = beforeHeight;
+			ray.Target = Position;
+			ray.TargetHeight = Height;
+			ray.CalculateEnd(new[] { Origin.Physics });
+
+			if ((beforePos - ray.End).Dist < (beforePos - Position).Dist)
+				Detonate(new Target(ray.End, ray.EndHeight));
+		}
+
+		public override List<string> Save()
+		{
+			var list = base.Save();
+			list.AddRange(SaveAttribute.GetFields(this, false));
+
+			return list;
+		}
+	}
+}
