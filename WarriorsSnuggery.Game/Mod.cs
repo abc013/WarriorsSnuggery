c77ff19e428b56c96e7e1714681880ab1345058f
abc013 <abc013@alpenjodel.de>
2022-04-02 00:00:47 +0200

    Read mod metadata out of file

--- WarriorsSnuggery.Game/Mod.cs
+++ WarriorsSnuggery.Game/Mod.cs
@@ -1,24 +1,30 @@
-﻿namespace WarriorsSnuggery
+﻿using System.Collections.Generic;
+using WarriorsSnuggery.Loader;
+
+namespace WarriorsSnuggery
 {
 	public class Mod
 	{
+		[Require]
 		public readonly string Name;
+		[Require]
 		public readonly string InternalName;
 
 		public readonly string Description = "No description.";
 		public readonly string Author = "Unknown author.";
-		public readonly string Version = "v?";
+		public readonly string Version = "Dev";
 		public readonly string GameVersion = "Unknown";
 
 		public readonly string Directory;
+		public readonly List<TextNode> Rules;
 
-		public Mod(string directory)
+		public Mod(string filepath)
 		{
-			Directory = directory;
-			InternalName = FileExplorer.FileName(directory);
-			Name = FileExplorer.FileName(directory);
+			Directory = FileExplorer.FileDirectory(filepath);
 
-			// TODO
+			Rules = TextNodeLoader.FromFile(Directory, FileExplorer.FileName(filepath) + FileExplorer.FileExtension(filepath));
+
+			TypeLoader.SetValues(this, Rules.Find(n => n.Key == "Mod").Children);
 		}
 	}
 }
--- WarriorsSnuggery.Game/ModManager.cs
+++ WarriorsSnuggery.Game/ModManager.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace WarriorsSnuggery
 {
@@ -10,12 +12,32 @@ namespace WarriorsSnuggery
 
 		public static void Load()
 		{
-			Core = new Mod(FileExplorer.MainDirectory);
+			// Core = new Mod(FileExplorer.MainDirectory);
 
 			foreach (var directory in FileExplorer.DirectoriesIn(FileExplorer.Mods))
 			{
-				Mods.Add(new Mod(directory));
+				var filepath = directory + FileExplorer.Separator + "Rules.yaml";
+
+				if (File.Exists(filepath))
+					Mods.Add(new Mod(filepath));
 			}
+		}
+
+		public List<Mod> GetActiveMods()
+		{
+			var mods = new List<Mod>();
+
+			foreach (var name in Settings.ModList)
+			{
+				var mod = Mods.FirstOrDefault(m => m.InternalName == name);
+
+				if (mod != null)
+					mods.Add(mod);
+				else
+					Log.Warning($"Unable to fetch unknown mod '{name}'. Skipping.");
+			}
+
+			return mods;
 		}
 
 		public static void Reload()
