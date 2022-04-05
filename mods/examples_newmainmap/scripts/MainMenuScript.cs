43384d51417817fd9ad409f95a2b79b1a6e17672
abc013 <abc013@alpenjodel.de>
2022-04-05 22:45:08 +0200

    Add new mainmap example mod

---
+++ mods/examples_newmainmap/scripts/MainMenuScript.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using WarriorsSnuggery;
+using WarriorsSnuggery.Loader;
+using WarriorsSnuggery.Objects.Particles;
+using WarriorsSnuggery.Scripting;
+
+namespace WarriorsSnuggeryScripts
+{
+	public class MainMenuScript : MissionScriptBase
+	{
+		readonly List<ParticleEntity> entities = new List<ParticleEntity>();
+
+		public MainMenuScript(PackageFile packageFile, Game game) : base(packageFile, game)
+		{
+			Tick += tick;
+		}
+
+		void tick()
+		{
+			if (Program.SharedRandom.Next(100) > 98)
+				addEntity();
+
+			foreach (var entity in entities)
+				entity.Tick();
+
+			entities.RemoveAll(e => !e.Alive);
+		}
+
+		void addEntity()
+		{
+			static CPos randomPosition(Random random, CPos bounds, int clamp) => new CPos(random.Next(clamp, bounds.X - clamp), random.Next(clamp, bounds.Y - clamp), 0);
+
+			var random = Program.SharedRandom;
+			var bounds = world.Map.Bounds.ToCPos();
+			var clamp = 4096;
+
+			entities.Add(new ParticleEntity(world, randomPosition(random, bounds, clamp), ((float)random.NextDouble()) * Angle.MaxRange));
+		}
+
+		class ParticleEntity
+		{
+			readonly World world;
+
+			readonly ParticleType smoke;
+			readonly ParticleType flame;
+
+			CPos position;
+			int height;
+
+			readonly float angle;
+			const int speed = 32;
+
+			public bool Alive => height > 0;
+
+			public ParticleEntity(World world, CPos position, float angle)
+			{
+				this.world = world;
+				this.position = position;
+				this.angle = angle;
+
+				height = 10240;
+				smoke = ParticleCache.Types["blood"];
+				flame = ParticleCache.Types["puff1"];
+			}
+
+			public void Tick()
+			{
+				position += CPos.FromFlatAngle(angle, speed);
+				height -= speed;
+
+				world.Add(new Particle(world, new ParticleInit(smoke, position, height)));
+
+				world.Add(new Particle(world, new ParticleInit(flame, position, height)));
+			}
+		}
+	}
+}
