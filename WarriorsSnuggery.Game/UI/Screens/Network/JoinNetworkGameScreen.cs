1e6a888dab8eb13f484d407d76c13a2707fc9e8c
abc013 <abc013@alpenjodel.de>
2022-08-27 02:56:46 +0200

    Adapt JoinNetworkGameScreen to UIPos changes

--- WarriorsSnuggery.Game/UI/Screens/Network/JoinNetworkGameScreen.cs
+++ WarriorsSnuggery.Game/UI/Screens/Network/JoinNetworkGameScreen.cs
@@ -15,61 +15,61 @@ namespace WarriorsSnuggery.UI.Screens
 		public JoinNetworkGameScreen(Game game) : base("Join Server")
 		{
 			this.game = game;
-			Title.Position = new CPos(0, -4096, 0);
+			Title.Position = new UIPos(0, -4096);
 
-			var join = new UIText(FontManager.Default, TextOffset.MIDDLE) { Position = new CPos(0, -2048, 0) };
+			var join = new UIText(FontManager.Default, TextOffset.MIDDLE) { Position = new UIPos(0, -2048) };
 			join.SetText("Select a username, then directly connect to a server.");
 			Add(join);
 
-			connection = new UIText(FontManager.Default, TextOffset.MIDDLE) { Position = new CPos(0, -1024, 0) };
+			connection = new UIText(FontManager.Default, TextOffset.MIDDLE) { Position = new UIPos(0, -1024) };
 			Add(connection);
 
-			var usernameText = new UIText(FontManager.Default, TextOffset.RIGHT) { Position = new CPos(-4096, 0, 0) };
+			var usernameText = new UIText(FontManager.Default, TextOffset.RIGHT) { Position = new UIPos(-4096, 0) };
 			usernameText.SetText("Username: ");
 			Add(usernameText);
 
 			username = new TextBox("wooden", 14)
 			{
-				Position = new CPos(1024, 0, 0),
+				Position = new UIPos(1024, 0),
 				Text = Settings.Name
 			};
 			Add(username);
 
-			var addressText = new UIText(FontManager.Default, TextOffset.RIGHT) { Position = new CPos(-4096, 2048, 0) };
+			var addressText = new UIText(FontManager.Default, TextOffset.RIGHT) { Position = new UIPos(-4096, 2048) };
 			addressText.SetText("Address: ");
 			Add(addressText);
 
 			address = new TextBox("wooden", 16)
 			{
-				Position = new CPos(0, 2048, 0),
+				Position = new UIPos(0, 2048),
 				Text = NetworkUtils.DefaultAddress
 			};
 			Add(address);
 
-			var portText = new UIText(FontManager.Default, TextOffset.RIGHT) { Position = new CPos(-4096, 3072, 0) };
+			var portText = new UIText(FontManager.Default, TextOffset.RIGHT) { Position = new UIPos(-4096, 3072) };
 			portText.SetText("Port: ");
 			Add(portText);
 
 			port = new TextBox("wooden", 5, InputType.NUMBERS)
 			{
-				Position = new CPos(0, 3072, 0),
+				Position = new UIPos(0, 3072),
 				Text = NetworkUtils.DefaultPort.ToString()
 			};
 			Add(port);
 
-			var passwordText = new UIText(FontManager.Default, TextOffset.RIGHT) { Position = new CPos(-4096, 4096, 0) };
+			var passwordText = new UIText(FontManager.Default, TextOffset.RIGHT) { Position = new UIPos(-4096, 4096) };
 			passwordText.SetText("Password: ");
 			Add(passwordText);
 
 			password = new TextBox("wooden", 16)
 			{
-				Position = new CPos(1024, 4096, 0),
+				Position = new UIPos(1024, 4096),
 				EmptyText = "Leave empty if none"
 			};
 			Add(password);
 
-			Add(new Button("Cancel", "wooden", () => game.ShowScreen(ScreenType.DEFAULT, false)) { Position = new CPos(-4096, 6144, 0) });
-			Add(new Button("Join", "wooden", () => tryConnect(address.Text, port.Text)) { Position = new CPos(4096, 6144, 0) });
+			Add(new Button("Cancel", "wooden", () => game.ShowScreen(ScreenType.DEFAULT, false)) { Position = new UIPos(-4096, 6144) });
+			Add(new Button("Join", "wooden", () => tryConnect(address.Text, port.Text)) { Position = new UIPos(4096, 6144) });
 		}
 
 		void tryConnect(string address, string port)
